9cb8192b4fa4787ff72d94bf7ea0086c82798072
ad5749 <heero44.tw@yahoo.com.tw>
2021-06-27 16:34:04 +0800

    提交作業Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo1';
+  title = 'app';
+  url = 'http://blog.miniasp.com/';
+  imgurl = '/assets/images/logo.png';
+  totalWords = 0;
+  constructor(){
+    // setTimeout(() => {
+    //   this.title = "The Will Will Web";
+    // }, 2000);
+  }
+  changeTitle(altKey:boolean){
+    if(altKey){
+      this.title = "The Will Will Web";
+    }
+  }
+  calcWords($event:any){
+    // console.log($event);
+    this.totalWords = $event.target.value.length;
+  }
+  clearnWords($event:any){
+    // console.log($event);
+    if($event.key == "Escape"){
+      $event.target.value = "";
+      this.totalWords = $event.target.value.length;
+    }
+  }
 }
